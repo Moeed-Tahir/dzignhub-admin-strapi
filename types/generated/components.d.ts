932623296008a5f6c095bd891adff204bc0b4a45
Usman Rasool <usmanrasool717@gmail.com>
2025-08-14 02:37:09 +0500

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,309 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlogPageBlogSection extends Struct.ComponentSchema {
+  collectionName: 'components_blog_page_blog_sections';
+  info: {
+    displayName: 'BlogSection';
+  };
+  attributes: {
+    heroSubtitle: Schema.Attribute.String;
+    heroTitle: Schema.Attribute.Text;
+    posts: Schema.Attribute.Component<'blog-page.post', true>;
+  };
+}
+
+export interface BlogPagePost extends Struct.ComponentSchema {
+  collectionName: 'components_blog_page_posts';
+  info: {
+    displayName: 'post';
+  };
+  attributes: {
+    conclusion: Schema.Attribute.Text;
+    contentAfterQuote: Schema.Attribute.Text;
+    contentFinal: Schema.Attribute.Text;
+    contentIntro: Schema.Attribute.Text;
+    contentResources: Schema.Attribute.Text;
+    contentResourcesList: Schema.Attribute.Component<
+      'landing-page.array',
+      true
+    >;
+    date: Schema.Attribute.Date;
+    excerpt: Schema.Attribute.Text;
+    featuredImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    image1: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    image1Reference: Schema.Attribute.String;
+    image2: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    image2Reference: Schema.Attribute.String;
+    imageHeader: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    link: Schema.Attribute.String;
+    quote: Schema.Attribute.Text;
+    quoteReference: Schema.Attribute.String;
+    slug: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface ContactPageForm extends Struct.ComponentSchema {
+  collectionName: 'components_contact_page_forms';
+  info: {
+    displayName: 'form';
+  };
+  attributes: {
+    contactImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    emailLabel: Schema.Attribute.String;
+    emailPlaceholder: Schema.Attribute.String;
+    failureMessage: Schema.Attribute.String;
+    firstNameLabel: Schema.Attribute.String;
+    firstNamePlaceholder: Schema.Attribute.String;
+    formTitle: Schema.Attribute.String;
+    lastNameLabel: Schema.Attribute.String;
+    lastNamePlaceholder: Schema.Attribute.String;
+    messageLabel: Schema.Attribute.String;
+    messagePlaceholder: Schema.Attribute.String;
+    submitButtonLabel: Schema.Attribute.String;
+    successMessage: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageArray extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_arrays';
+  info: {
+    displayName: 'array';
+  };
+  attributes: {
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageAssistantItem extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_assistant_items';
+  info: {
+    displayName: 'AssistantItem';
+  };
+  attributes: {
+    avatar: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    group: Schema.Attribute.Enumeration<['top', 'bottom', 'middle']>;
+    name: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageAssistantSection extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_assistant_sections';
+  info: {
+    displayName: 'AssistantSection';
+  };
+  attributes: {
+    assistants: Schema.Attribute.Component<'landing-page.assistant-item', true>;
+    headingHighlight: Schema.Attribute.String;
+    headingPost: Schema.Attribute.String;
+    headingPre: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageCardItem extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_card_items';
+  info: {
+    displayName: 'CardItem';
+  };
+  attributes: {
+    borderImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    description: Schema.Attribute.Text;
+    features: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageCarousal extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_carousals';
+  info: {
+    displayName: 'Carousal';
+  };
+  attributes: {
+    images1: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    images2: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+  };
+}
+
+export interface LandingPageDownloadSection extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_download_sections';
+  info: {
+    displayName: 'DownloadSection';
+  };
+  attributes: {
+    appStoreImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    appStoreUrl: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    googlePlayImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    googlePlayUrl: Schema.Attribute.String;
+    phoneImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    preTitle: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_hero_sections';
+  info: {
+    displayName: 'HeroSection';
+  };
+  attributes: {};
+}
+
+export interface LandingPagePlanItem extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_plan_items';
+  info: {
+    displayName: 'PlanItem';
+  };
+  attributes: {
+    benefits: Schema.Attribute.Component<'landing-page.array', true>;
+    buttonLabel: Schema.Attribute.String;
+    link: Schema.Attribute.String;
+    plan: Schema.Attribute.String;
+    price: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageStackCard extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_stack_cards';
+  info: {
+    displayName: 'StackCard';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    num: Schema.Attribute.String;
+    para: Schema.Attribute.Text;
+    type: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageStackSection extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_stack_sections';
+  info: {
+    displayName: 'StackSection';
+  };
+  attributes: {
+    card: Schema.Attribute.Component<'landing-page.stack-card', true>;
+    subtitle: Schema.Attribute.Text;
+    titleline1: Schema.Attribute.String;
+    titleline2: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageTemplateItem extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_template_items';
+  info: {
+    displayName: 'TemplateItem';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    images: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    layout: Schema.Attribute.Enumeration<
+      ['website', 'lummi', 'brands', 'social ', 'slides']
+    >;
+    slides: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageTestimonials extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_testimonials';
+  info: {
+    displayName: 'testimonials';
+  };
+  attributes: {
+    avatar: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    layoutVariant: Schema.Attribute.Enumeration<
+      ['variant-1', 'variant-2', 'variant-3']
+    >;
+    name: Schema.Attribute.String;
+    quote: Schema.Attribute.Text;
+    role: Schema.Attribute.String;
+  };
+}
+
+export interface LandingPageTestimonialsSection extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_testimonials_sections';
+  info: {
+    displayName: 'TestimonialsSection';
+  };
+  attributes: {
+    headingMain: Schema.Attribute.String;
+    headingPre: Schema.Attribute.String;
+    testimonial: Schema.Attribute.Component<'landing-page.testimonials', true>;
+  };
+}
+
+export interface LandingPageWorkCard extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_work_cards';
+  info: {
+    displayName: 'WorkCard';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    desktopManualImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    desktopProImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    mobileManualImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    mobileProImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blog-page.blog-section': BlogPageBlogSection;
+      'blog-page.post': BlogPagePost;
+      'contact-page.form': ContactPageForm;
+      'landing-page.array': LandingPageArray;
+      'landing-page.assistant-item': LandingPageAssistantItem;
+      'landing-page.assistant-section': LandingPageAssistantSection;
+      'landing-page.card-item': LandingPageCardItem;
+      'landing-page.carousal': LandingPageCarousal;
+      'landing-page.download-section': LandingPageDownloadSection;
+      'landing-page.hero-section': LandingPageHeroSection;
+      'landing-page.plan-item': LandingPagePlanItem;
+      'landing-page.stack-card': LandingPageStackCard;
+      'landing-page.stack-section': LandingPageStackSection;
+      'landing-page.template-item': LandingPageTemplateItem;
+      'landing-page.testimonials': LandingPageTestimonials;
+      'landing-page.testimonials-section': LandingPageTestimonialsSection;
+      'landing-page.work-card': LandingPageWorkCard;
+    }
+  }
+}
